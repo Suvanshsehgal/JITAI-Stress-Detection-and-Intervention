713b21818bdbff589c027c580c448b30fdfe3c83
Suvanshsehgal <suvanshsehgal@gmail.com>
2026-05-04 12:04:44 +0530

    chore: update Supabase config

--- stressdata/lib/config/supabase_config.dart
+++ stressdata/lib/config/supabase_config.dart
@@ -3,8 +3,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseConfig {
   static Future<void> init() async {
     await Supabase.initialize(
-      url: 'https://eojegpdmooxwmrqkcich.supabase.co',
-      anonKey: 'sb_publishable_l5atLaK61dDLwKs17EABcA_Wrny-rzV',
+      url: 'https://vjbhmyyeqenlypyqufgd.supabase.co',
+      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqYmhteXllcWVubHlweXF1ZmdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MjU3ODQsImV4cCI6MjA5MzQwMTc4NH0.WoMAmiDnaEFs-Ad_WuX37tHlajGSQIAdxv2jf6X2eU4',
     );
   }
 
